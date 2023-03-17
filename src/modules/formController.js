f3366dcbd7234fd147dbaf1e18e496ae9b2e2304
NikSkor <mr-zerx@yandex.ru>
2023-03-17 05:19:40 +0500

    Update formController.js

--- src/modules/formController.js
+++ src/modules/formController.js
@@ -12,11 +12,7 @@ export const formController = async (form, formSearch) => {
       validateForm(searchRequest);
     });
     return;
-  }
-
-  // if(arrResult === undefined) {
-  //   arrResult = null;
-  // }
+  };
 
   let arrResult = await getRepo(searchRequest.value);
 
