f17237cc3a2175bdaeec62f5eb220176399d86d7
NikSkor <mr-zerx@yandex.ru>
2023-03-17 15:01:10 +0500

    update

--- src/modules/validateForm.js
+++ src/modules/validateForm.js
@@ -5,18 +5,16 @@ export const validateForm = (input) => {
   let warn = parent.querySelector('.form__warning');
   let text = '';
 
-  if (input.value.length > 256) {
-
+  if (input.value.length <= 2) {
+    if (warn !== null) warn.remove();;
+    text = 'Введено менее 3 символов';
+    renderWarn(text, parent);
+    return;
+  } else if (input.value.length > 256) {
+    if (warn !== null) warn.remove();;  
     text = 'Значение не должно превышать 256 символов';
     renderWarn(text, parent);
-  }
-
-  if (input.value === '') {
-    if (warn !== null) return;
-
-    text = 'Поле не заполнено';
-
-    renderWarn(text, parent);
+    return;
   } else {
     if (warn === null) return;
 
